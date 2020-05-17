8e5676e5916555fb5e5b3854c4bbd3d0f59b920f
wimstockman <wim.stockman@gmail.com>
2020-05-17 21:42:41 +0200

    changed tag icons

--- config.def.h
+++ config.def.h
@@ -30,7 +30,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "☯", "", "", "", "", "", "", "", "𐝠" };
+static const char *tags[] = { "","☯", "\ue005", "", "", "", "\uf263", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
